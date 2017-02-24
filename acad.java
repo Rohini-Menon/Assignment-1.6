78deadf961e63ddf7fce10b108215dae889798a8
Rohini-Menon <rohinimenon1995@gmail.com>
2017-02-24 11:01:30 +0530

    Add files via upload

--- acad.java
+++ acad.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 
 public class acad {
 
+	private static Scanner sc;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x=10;
-		int y=25;
-		int sum;
-		sum=x+y;
+		sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=a+b;
 		System.out.println(sum);
 
 	}
