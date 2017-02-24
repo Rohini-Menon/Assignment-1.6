33e6778258e0602156ec37ebd84e7b79feffb7c6
Rohini-Menon <rohinimenon1995@gmail.com>
2017-02-24 10:47:41 +0530

    Add files via upload

---
+++ acad.java
@@ -0,0 +1,14 @@
+
+public class acad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=25;
+		int sum;
+		sum=x+y;
+		System.out.println(sum);
+
+	}
+
+}
